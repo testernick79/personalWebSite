8fefc095286198088a9e0261e9ee849b8c8ff6d7
Nick Fonseca <tester.nick79@gmail.com>
2019-06-02 02:11:20 -0700

    all functionality requirements done

--- L10HandsOn/README.md
+++ L10HandsOn/README.md
@@ -19,11 +19,11 @@ To begin, create a new project in your FrontEndFoundations folder named L10Hands
 
 * List your tech-related skills.
 
-Dynamically list your GitHub repositories using an AJAX request.
+* Dynamically list your GitHub repositories using an AJAX request.
 You will need the following API endpoint with your username. https://api.github.com/users/YOURUSERNAME/repos
 Hint! Post the above into the web browser and you will see all of your repos listed as a JSON object
 
-Include a footer that sticks to the bottom of the screen, commonly referred to as a sticky footer.
+* Include a footer that sticks to the bottom of the screen, commonly referred to as a sticky footer.
 The footer should contain contact information with an email icon and an email address listed next to it.
 
 Style your page using CSS.
--- L10HandsOn/script.js
+++ L10HandsOn/script.js
@@ -1,20 +1,54 @@
 $(document).ready(function () {
     $('.movement').hover(function () {
-		$(this).css('font-size', '35px'),
-		    $(this).css('padding', '3.5px');
-		},
-		function () {
-		    $(this).css('font-size', '20px'),
-		        $(this).css('padding', '10px');
-    });
+            $(this).css('font-size', '35px'),
+                $(this).css('padding', '3.5px');
+        },
+        function () {
+            $(this).css('font-size', '20px'),
+                $(this).css('padding', '10px');
+        });
 });
-        
-let gitResuest =  new XMLHttpRequest();
-gitResuest.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-        let gitObj = JSON.parse(this.responseText);
-        document.getElementById("repo").innerHTML.repos_url;
+
+/* function loadMyRepo() {
+
+    let requestGitter = new XMLHttpRequest();
+    requestGitter.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let gitObj = JSON.parse(this.responseText);
+            for (var i = 0; i < gitObj.length; i++) {
+                var ul = document.getElementById("repo");
+                var li = document.createElement("li");
+                var a = document.createElement("a");
+
+                li.appendChild(document.createTextNode(gitObj[i].name));
+                a.appendChild(li);
+                a.setAttribute("href", gitObj[i].repos_url);
+                ul.appendChild(a);
+            }
+        }
     }
 };
-gitResuest.open("GET", "https://api.github.com/users/testernick79", true);
-gitResuest.send(); 
+requestGitter.open("GET", "https://api.github.com/users/testernick79", true);
+requestGitter.send(); */
+
+function loadRepo() {
+
+    let gitHubRequest = new XMLHttpRequest();
+    gitHubRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let gitObject = JSON.parse(this.responseText);
+            for (var i = 0; i < gitObject.length; i++) {
+                var ul = document.getElementById("gitRepositories");
+                var li = document.createElement("LI");
+                var a = document.createElement("a");
+
+                li.appendChild(document.createTextNode(gitObject[i].name));
+                a.appendChild(li);
+                a.setAttribute("href", gitObject[i].html_url);
+                ul.appendChild(a);
+            }
+        }
+    };
+    gitHubRequest.open("GET", "https://api.github.com/users/testernick79/repos", true);
+    gitHubRequest.send();
+}
